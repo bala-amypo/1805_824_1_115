069b7c69c99aa1f465861373aa2ebd71633c4391
bala-amypo <bala@amypo.in>
2025-12-18 09:36:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/AlertRecordRepository.java
@@ -3,4 +3,4 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.AlertRecord;
 
-public interface 
+public interface AlertRecordRepository extends JpaRepository<AlertRecord,Long>
---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+
+import org.sprinframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.UserRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>
