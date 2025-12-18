23ffc7dace37e991e1825914fe1f9a8718ddd736
bala-amypo <bala@amypo.in>
2025-12-18 09:57:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertRecord.java
+++ src/main/java/com/example/demo/entity/AlertRecord.java
@@ -52,4 +52,5 @@ public class AlertRecord {
 
     public Boolean getAcknowledged() { return acknowledged; }
     public void setAcknowledged(Boolean acknowledged) { this.acknowledged = acknowledged; }
+    
 }
