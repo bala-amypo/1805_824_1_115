69884e6ae605f78d27ed730b7c2fbec950b1bb05
bala-amypo <bala@amypo.in>
2025-12-18 09:46:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/TemperatureSensorLogRepository.java
+++ src/main/java/com/example/demo/repository/TemperatureSensorLogRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.TemperatureSensorLog;
 
-public interface TemperatureSensorLogRepository extends JpaRepository<TemperatureSensorLogr,Long>{
+public interface TemperatureSensorLogRepository extends JpaRepository<TemperatureSensorLog,Long>{
 
 }
