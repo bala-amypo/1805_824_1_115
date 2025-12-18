1d1deff57893a3ffe299ff569063f330239eebb5
bala-amypo <bala@amypo.in>
2025-12-18 05:14:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/TemperatureSensorLog.java
@@ -0,0 +1,75 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class ShipmentRecord{
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Long shipmentld;
+    private String sensorld;
+    private String recordedAt;
+    private Double temperatureValue;
+    private String location;
+
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getShipmentld() {
+        return shipmentld;
+    }
+
+    public void setShipmentld(Long shipmentld) {
+        this.shipmentld = shipmentld;
+    }
+
+    public String getSensorld() {
+        return sensorld;
+    }
+
+    public void setSensorld(String sensorld) {
+        this.sensorld = sensorld;
+    }
+
+    public String getRecordedAt() {
+        return recordedAt;
+    }
+
+    public void setRecordedAt(String recordedAt) {
+        this.recordedAt = recordedAt;
+    }
+
+    public Double getTemperatureValue() {
+        return temperatureValue;
+    }
+
+    public void setTemperatureValue(Double temperatureValue) {
+        this.temperatureValue = temperatureValue;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
+
+
+}
