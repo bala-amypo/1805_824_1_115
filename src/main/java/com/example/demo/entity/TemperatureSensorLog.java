d17ca83e5e3e4b16a38f3c28c8dd199a885e11da
bala-amypo <bala@amypo.in>
2025-12-18 05:19:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/TemperatureSensorLog.java
+++ src/main/java/com/example/demo/entity/TemperatureSensorLog.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 
 
 @Entity
-public class ShipmentRecord{
+public class TemperatureSensorLog{
 @Id
 @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
