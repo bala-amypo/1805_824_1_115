feedaafa727db6be66dc9004446a3778765791cf
bala-amypo <bala@amypo.in>
2025-12-18 09:26:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/ShipmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/ShipmentRecordRepository.java
@@ -3,8 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.ShipmentRecord;
 
-public interface ShipmentRecordRepository extends JpaRepository<ShipmentRecord,Long>(){
-
-}
+public interface ShipmentRecordRepository extends JpaRepository<ShipmentRecord,Long>
 
 
--- src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
+++ src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
@@ -1,4 +1,8 @@
-package com.example.
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ShipmentRecord;
+import com.example.demo.entity.TemperatureRule;
+
+public interface TemperatureRuleRepository extends JpaRepository<TemperatureRule,Long>(){
+
+}
