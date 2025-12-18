2c39fe034dad77cf6f0291848b30c1cad1702c36
bala-amypo <bala@amypo.in>
2025-12-18 10:36:43 +0000

    User commit

--- src/main/java/com/example/demo/service/TemperatureRuleService.java
+++ src/main/java/com/example/demo/service/TemperatureRuleService.java
@@ -1,1 +1,8 @@
-package com.example.demo.service
+package com.example.demo.service;
+import com.example.demo.entity.TemperatureRule;
+import jakarta.persistence.Service;
+
+@Service
+public interface TemperatureRule{
+    public TemperatureRule createRule(TemperatureRule rule)
+}
