e6974892bb1a699c25fa4c8b3b4fd6a99499e851
bala-amypo <bala@amypo.in>
2025-12-18 08:09:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/TemperatureRule.java
+++ src/main/java/com/example/demo/entity/TemperatureRule.java
@@ -1,105 +1,65 @@
-package com.example.demo.entity;
+package com.example.collectiondp.entity;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "temperature_rule")
 public class TemperatureRule {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String productType;
-
-    @Column(nullable = false)
     private Double minTemp;
-
-    @Column(nullable = false)
-    private Double maxTemp;
-
-    @Column(nullable = false)
+    private Double maxtTemp;
     private Boolean active;
-
-    @Column(nullable = false)
     private LocalDate effectiveFrom;
-
-    @Column(nullable = true)
     private LocalDate effectiveTo;
-
-    @PrePersist
-    public void prePersist() {
-        if (effectiveFrom == null) {
-            effectiveFrom = LocalDate.now();
-        }
-        if (active == null) {
-            active = true;
-        }
-    }
-
-    // Getters & Setters
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getProductType() {
         return productType;
     }
-
     public void setProductType(String productType) {
         this.productType = productType;
     }
-
     public Double getMinTemp() {
         return minTemp;
     }
-
     public void setMinTemp(Double minTemp) {
         this.minTemp = minTemp;
     }
-
-    public Double getMaxTemp() {
-        return maxTemp;
+    public Double getMaxtTemp() {
+        return maxtTemp;
     }
-
-    public void setMaxTemp(Double maxTemp) {
-        this.maxTemp = maxTemp;
+    public void setMaxtTemp(Double maxtTemp) {
+        this.maxtTemp = maxtTemp;
     }
-
     public Boolean getActive() {
         return active;
     }
-
     public void setActive(Boolean active) {
         this.active = active;
     }
-
     public LocalDate getEffectiveFrom() {
         return effectiveFrom;
     }
-
     public void setEffectiveFrom(LocalDate effectiveFrom) {
         this.effectiveFrom = effectiveFrom;
     }
-
     public LocalDate getEffectiveTo() {
         return effectiveTo;
     }
-
     public void setEffectiveTo(LocalDate effectiveTo) {
         this.effectiveTo = effectiveTo;
     }
+
+    
 }
