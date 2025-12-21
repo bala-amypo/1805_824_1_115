e6d71d93a1c821a07dca7f00f709764bbf5ffd11
bala-amypo <bala@amypo.in>
2025-12-21 13:52:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertController.java
+++ src/main/java/com/example/demo/controller/AlertController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.example.demo.entity.AlertRecord;
 import com.example.demo.service.AlertService;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,22 +16,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AlertController {
 
     @Autowired
-    private AlertService as;
-
-  
-    @PostMapping("/addalert")
-    public AlertRecord createAlert(@RequestBody AlertRecord alert) {
-        return as.triggerAlert(alert);
-    }
-
-    @GetMapping("/getallalerts")
-    public List<AlertRecord> getAllAlerts() {
-        return as.getAllAlerts();
-    }
+    private AlertService alertService;
 
     
-    @GetMapping("/alerts/{shipmentId}")
-    public List<AlertRecord> getAlertsByShipment(@PathVariable Long shipmentId) {
-        return as.getAlertsByShipment(shipmentId);
+    @PostMapping("/addalert")
+    public AlertRecord create(@RequestBody AlertRecord alert) {
+        return alertService.triggerAlert(alert);
+    }
+ 
+    @GetMapping("/getallalerts")
+    public List<AlertRecord> listall() {
+        return alertService.getAllAlerts();
+    }
+
+     
+    @GetMapping("/getalert/{shipmentId}")
+    public List<AlertRecord> getAlert(@PathVariable Long shipmentId) {
+        return alertService.getAlert(shipmentId);
     }
 }
