1a3167833f218c2edd207d7a33d3b2f8ba67e2cc
bala-amypo <bala@amypo.in>
2025-12-21 05:21:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
@@ -1,16 +1,16 @@
 package com.example.demo.service.impl;
-import com.example.demo.repository.ShipmentRecordRepository;
-import com.example.demo.entity.ShipmentRecord;
-import com.example.demo.service.ShipmentRecordService;
+import com.example.demo.repository.AlertRecordRepository;
+import com.example.demo.entity.AlertRecord;
+import com.example.demo.service.AlertRecordService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
-public class ShipmentRecordServiceImp implements ShipmentRecordService{
+public class AlertRecordServiceImp implements AlertRecordService{
 
     @Autowired
-    ShipmentRecordRepository smr;
-    public ShipmentRecord createShipmentRecord(ShipmentRecord record){
-        return smr.save(record);
+    AlertRecordRepository arr;
+    public ShipmentRecord createAlertRecord(AlertRec record){
+        return arr.save(record);
     }
 }
