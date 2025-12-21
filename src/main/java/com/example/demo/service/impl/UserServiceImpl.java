f1553a34b7c15604cdbe438f039c5042ab0abb37
bala-amypo <bala@amypo.in>
2025-12-21 05:30:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,16 +1,16 @@
 package com.example.demo.service.impl;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.entity.User;
-import com.example.demo.service.UserService;
+import com.example.demo.repository.ShipmentRecordRepository;
+import com.example.demo.entity.ShipmentRecord;
+import com.example.demo.service.ShipmentRecordService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
-public class UserServiceImp implements UserService{
+public class ShipmentRecordServiceImp implements ShipmentRecordService{
 
     @Autowired
-    UserRepository smr;
-    public User createShipmentRecord(ShipmentRecord record){
+    ShipmentRecordRepository smr;
+    public ShipmentRecord createShipmentRecord(ShipmentRecord record){
         return smr.save(record);
     }
 }
