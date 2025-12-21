2bc8176a6d9274922b561fbca912391bf48b6d34
bala-amypo <bala@amypo.in>
2025-12-21 05:06:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/ShipmentRecordController.java
+++ src/main/java/com/example/demo/controller/ShipmentRecordController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.example.demo.Entityclass.User;
-import com.example.demo.service.UserService;
+import com.example.demo.service.ShipmentReService;
 import java.util.List;
 
 
 @RestController
 public class ShipmentRecordController{
     @Autowired
-    ShipmentRecordService smr;
+    ShipmentRecordService sms;
     @PostMapping("/rules")
     public User rules(@RequestBody ShipmentRecord record)
     {
-        return crs.ShipmentRecord(record);
+        return sms.ShipmentRecord(record);
     }
     @GetMapping
     public List<User> getrules()
