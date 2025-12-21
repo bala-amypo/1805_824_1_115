488dc8fbcbb9445ff1ff142d93641402750679a1
bala-amypo <bala@amypo.in>
2025-12-21 05:46:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
-import com.example.demo.repository.AlertRecordRepository;
-import com.example.demo.entity.AlertRecord;
-import com.example.demo.service.AlertRecordService;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
