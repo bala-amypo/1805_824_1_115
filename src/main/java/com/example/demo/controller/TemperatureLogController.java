39b367dbd27bd4adac076f0dd199caedbc8c0821
bala-amypo <bala@amypo.in>
2025-12-21 15:15:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TemperatureLogController.java
@@ -0,0 +1,14 @@
+ package com.example.demo.TemperatureLogController;
+import java.util.List;
+import com.example.demo.entity.TemperatureSensorLog;
+import com.example.demo.service.AlertService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class TemperatureLog
