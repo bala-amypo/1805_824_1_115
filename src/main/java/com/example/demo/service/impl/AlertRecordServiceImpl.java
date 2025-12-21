e81e213c7bfd699e17c2d070177e64d675f68538
bala-amypo <bala@amypo.in>
2025-12-21 13:31:36 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertRecordServiceImpl.java
@@ -4,17 +4,17 @@ import com.example.demo.entity.AlertRecord;
 import com.example.demo.service.AlertRecordService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.list;
+import java.util.List;
 
 @Service
 public class AlertRecordServiceImpl implements AlertRecordService{
 
     @Autowired
     AlertRecordRepository arr;
-    public triggerAlert(AlertRecord alert){
+    public AlertRecord triggerAlert(AlertRecord alert){
         return arr.save(alert);
     }
-    public List<AlertRecord> getAlertsByShipment(){
+    public List<AlertRecord> getAlertsByShipment(Long shipmentId){
         return arr.findById(shipmentId);
     }
     public List<AlertRecord> getAllAlerts(){
