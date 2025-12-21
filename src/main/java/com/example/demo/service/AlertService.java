fb036017ac92d593acc227c05c42d1a893666ea9
bala-amypo <bala@amypo.in>
2025-12-21 07:03:20 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertService.java
+++ src/main/java/com/example/demo/service/AlertService.java
@@ -1,10 +1,10 @@
 package com.example.demo.service;
-import com.example.demo.entity.AlertRecord;
-import jakarta.persistence.Service;
 
-public interface AlertService{
- public AlertRecord triggerAlert(AlertRecord alert);
- public AlertRecord acknowledgeAlert(Long id);
- public AlertRecord getAlertsByShipment(Long shipment);
- public AlertRecord getAllAlerts();
+import com.example.demo.entity.AlertRecord;
+import java.util.List;
+
+public interface AlertService {
+    AlertRecord triggerAlert(AlertRecord alert);
+    List<AlertRecord> getAlertsByShipment(Long shipmentId);
+    List<AlertRecord> getAllAlerts();
 }
