b4b63f73cb9489772e72b976f86b55a1f48d80b6
bala-amypo <bala@amypo.in>
2025-12-21 16:51:04 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TemperatureLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TemperatureLogServiceImpl.java
@@ -1,38 +1,37 @@
-package com.example.demo.controller;
+package com.example.demo.service.impl;
+
+import com.example.demo.repository.TemperatureSensorLogRepository;
+import com.example.demo.entity.TemperatureSensorLog;
+import com.example.demo.service.TemperatureLogService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
-import com.example.demo.entity.TemperatureSensorLog;
-import com.example.demo.service.TemperatureLogService;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-@RestController
-@RequestMapping("/temperature")
-public class TemperatureLogController {
+@Service
+public class TemperatureLogServiceImpl implements TemperatureLogService {
 
     @Autowired
-    private TemperatureLogService tm;
+    private TemperatureSensorLogRepository tsl;
 
-    @PostMapping("/log")
-    public TemperatureSensorLog create(@RequestBody TemperatureSensorLog log) {
-        return tm.recordLog(log);
+    @Override
+    public TemperatureSensorLog recordLog(TemperatureSensorLog log) {
+        return tsl.save(log);
     }
 
-    @GetMapping("/all")
-    public List<TemperatureSensorLog> listtemp() {
-        return tm.getAllLogs();
+    @Override
+    public List<TemperatureSensorLog> getAllLogs() {
+        return tsl.findAll();
     }
 
-    @GetMapping("/getid/{id}")
-    public Optional<TemperatureSensorLog> getbyid(@PathVariable Long id) {
-        return tm.getLogById(id);
+    @Override
+    public Optional<TemperatureSensorLog> getLogById(Long id) {
+        return tsl.findById(id);
     }
 
-    @GetMapping("/shipment/{shipmentId}")
-    public List<TemperatureSensorLog> getshipid(@PathVariable Long shipmentId) {
-        return tm.getLogsByShipment(shipmentId);
+    @Override
+    public List<TemperatureSensorLog> getLogsByShipment(Long shipmentId) {
+        return tsl.findByShipmentId(shipmentId);
     }
 }
