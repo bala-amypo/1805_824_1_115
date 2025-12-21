587f9261976151795a269b85b09b786a8f6b8657
bala-amypo <bala@amypo.in>
2025-12-21 16:24:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/TemperatureLogController.java
+++ src/main/java/com/example/demo/controller/TemperatureLogController.java
@@ -1,41 +1,41 @@
- package com.example.demo.TemperatureLogController;
+package com.example.demo.controller;
+
 import java.util.List;
+import java.util.Optional;
+
 import com.example.demo.entity.TemperatureSensorLog;
-import com.example.demo.service.AlertService;
+import com.example.demo.service.TemperatureLogService;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
-public class TemperatureLogController{
-    
+public class TemperatureLogController {
+
     @Autowired
     private TemperatureLogService tm;
 
     @PostMapping("/log")
-    public TemperatureSensorLog create(@RequestBody TemperatureSensorLog log){
-        return as.recordLog(sensor)
+    public TemperatureSensorLog create(@RequestBody TemperatureSensorLog log) {
+        return tm.recordLog(log);
     }
 
     @GetMapping("/addtemp")
-        public List<TemperatureSensorLog> listtemp(){
-            return as.getAllLogs();
-        }
-    
-        @GetMapping("/getid")
-            public Optional<TemperatureSensorLog>getbyid(@PathVariable Long id){
-                return as.getLogById(id);
-            }
+    public List<TemperatureSensorLog> listtemp() {
+        return tm.getAllLogs();
+    }
 
-            @GetMapping("/getshipmentid/{shipmentid}")
-            public Optional<TemperatureSensorLog> getshipid(@pathVariables Long shipmentId){
-                return as.getLogsByShipment(shipmentId);
-            }
-        
-    
+    @GetMapping("/getid/{id}")
+    public Optional<TemperatureSensorLog> getbyid(@PathVariable Long id) {
+        return tm.getLogById(id);
+    }
 
+    @GetMapping("/getshipmentid/{shipmentId}")
+    public List<TemperatureSensorLog> getshipid(@PathVariable Long shipmentId) {
+        return tm.getLogsByShipment(shipmentId);
+    }
 }
