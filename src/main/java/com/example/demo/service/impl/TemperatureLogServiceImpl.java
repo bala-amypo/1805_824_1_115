d6f918a3411dba1c289e6b89a733d003b54ed19e
bala-amypo <bala@amypo.in>
2025-12-25 14:43:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TemperatureLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TemperatureLogServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.TemperatureSensorLog;
 import com.example.demo.repository.TemperatureSensorLogRepository;
 import com.example.demo.service.TemperatureLogService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TemperatureLogServiceImpl implements TemperatureLogService {
 
     private final TemperatureSensorLogRepository repo;
