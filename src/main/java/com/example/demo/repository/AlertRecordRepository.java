52e18e72779dfc9fcc6f2c7567dc34b2e43bec74
bala-amypo <bala@amypo.in>
2025-12-25 13:31:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.AlertRecord;
-
-public interface AlertRecordRepository extends JpaRepository<AlertRecord,Long>{
-    List<AlertRecord> findByShipmentId(Long shipmentId);
-}
