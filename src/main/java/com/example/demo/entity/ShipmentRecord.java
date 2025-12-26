7648ea20b7d795ab107c2d4bf32edac0a0c16cb2
bala-amypo <bala@amypo.in>
2025-12-26 06:48:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/ShipmentRecord.java
+++ src/main/java/com/example/demo/entity/ShipmentRecord.java
@@ -1,10 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
 
 @Entity
 public class ShipmentRecord {
--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -3,17 +3,13 @@ package com.example.demo.servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-        resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType("text/plain");
-
         resp.getWriter().write("Cold Chain Temperature Breach Alert System is running");
         resp.getWriter().flush();
     }
