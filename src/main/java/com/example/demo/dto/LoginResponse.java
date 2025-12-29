784b029b25d0b3a46b37ad8c1202226267d73110
bala-amypo <bala@amypo.in>
2025-12-29 08:19:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/LoginResponse.java
@@ -0,0 +1,28 @@
+// package com.example.demo.dto;
+
+// public class JwtResponse {
+
+//     private String token;
+
+//     public JwtResponse(String token) {
+//         this.token = token;
+//     }
+
+//     public String getToken() {
+//         return token;
+//     }
+// }
+package com.example.demo.dto;
+
+public class LoginResponse {
+    private String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
+
